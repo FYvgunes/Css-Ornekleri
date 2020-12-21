80465ec0292eaa76566ae390223ff0b65450d727
veysel gunes <veysel.gunes36@gmail.com>
2020-12-22 01:01:36 +0300

    Fetch Api ile Random Resim çekme

---
+++ JavascriptOgreniyorum/js/lib.js
@@ -0,0 +1,14 @@
+export const Pi = 3.14;
+
+expert
+const sum = (x, y) => {
+    return x + y;
+}
+expert
+const heypotunes = (x, y) => {
+    return Math.squred(squred(x) + squred(y))
+}
+expert
+const squred = (x) => {
+    return x * x;
+}
---
+++ JavascriptOgreniyorum/main.js
@@ -0,0 +1,7 @@
+import * as lib from "./js/lib";
+
+document.querySelector("#result").innerHTML =
+    lib.sum(3, 4) + "<br>" +
+    lib.Pi(3) + "<br>" +
+    lib.squred(14) + "<br>" +
+    lib.heypotunes(3, 5) + "<br>";
