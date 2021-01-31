8e01d02b4af98dbbfac90c1987660a1568e1f14a
veysel gunes <veysel.gunes36@gmail.com>
2021-01-31 23:32:07 +0300

    Javascript File added

--- JavascriptOgreniyorum/weater-js/main.js
+++ JavascriptOgreniyorum/weater-js/main.js
@@ -1,76 +1,94 @@
 const api = {
-  key: "5d9e156fb8a5307802470209fa882947",
-  base: "https://api.openweathermap.org/data/2.5/",
+    key: "5d9e156fb8a5307802470209fa882947",
+    base: "https://api.openweathermap.org/data/2.5/",
 };
 
 const searchbox = document.querySelector(".search-box");
 searchbox.addEventListener("keypress", setQuery);
 
 function setQuery(evt) {
-  if (evt.keyCode == 13) {
-    getResult(searchbox.value);
-    console.log(searchbox.value);
-  }
+    if (evt.keyCode == 13) {
+        getResult(searchbox.value);
+
+    }
 }
 
 function getResult(query) {
-  fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
-    .then((weather) => {
-      return weather.json();
-    })
-    .then(displayResults);
+    fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+        .then((weather) => {
+            return weather.json();
+        })
+        .then(displayResults);
 }
 
 function displayResults(weather) {
-  console.log(weather);
-  let city = document.querySelector(".location .city");
-  city.innerText = `${weather.name}, ${weather.sys.country}`;
+    function status() {
+        let status = ["Güneşli", "Bulutlu", "Karlı", "Açık", "Yağmurlu", "Sisli"]
+        if (weather.weather[0].main == "Clouds") {
+            return status[1];
+        } else if (weather.weather[0].main == "Clear") {
+            return status[3];
+        } else if (weather.weather[0].main == "Mist") {
+            return status[5];
+        } else if (weather.weather[0].main == "Snow") {
+            return status[2];
+        } else if (weather.weather[0].main == "Rain") {
+            return status[4];
+        }
+    }
 
-  let now = new Date();
-  let date = document.querySelector(".location .date");
-  date.innerText = dateBuilder(now);
+    let city = document.querySelector(".location .city");
+    city.innerText = `${weather.name}, ${weather.sys.country}`;
 
-  let temp = document.querySelector(".current .temp");
-  temp.innerHTML = `${Math.round(weather.main.temp)} <span>°c</span>`;
+    let now = new Date();
+    let date = document.querySelector(".location .date");
+    date.innerText = dateBuilder(now);
 
-  let weather_el = document.querySelector(".current .weather");
-  weather_el.innerText = weather.weather[0].main;
+    let temp = document.querySelector(".current .temp");
+    temp.innerHTML = `${Math.round(weather.main.temp)} <span>°c</span>`;
 
-  let hilow = document.querySelector(".current .hi-low");
-  hilow.innerHTML = `${Math.round(weather.main.temp_min)}°c/ ${Math.round(
+    let weather_el = document.querySelector(".current .weather");
+    weather_el.innerText = status();
+
+    let hilow = document.querySelector(".current .hi-low");
+    hilow.innerHTML = `${Math.round(weather.main.temp_min)}°c/ ${Math.round(
     weather.main.temp_max
   )}°c`;
+
 }
 
+
+
+
 function dateBuilder(d) {
-  let months = [
-    "Ocak",
-    "Şubat",
-    "Mart",
-    "Nisan",
-    "Mayıs",
-    "haziran",
-    "temmuz",
-    "Ağustos",
-    "Eylül",
-    "Ekim",
-    "Kasım",
-    "Aralık",
-  ];
-  let days = [
-    "Pazartesi",
-    "Salı",
-    "Çarşamba",
-    "Perşembe",
-    "Cuma",
-    "Cumartesi",
-    "Pazar",
-  ];
+    let months = [
+        "Ocak",
+        "Şubat",
+        "Mart",
+        "Nisan",
+        "Mayıs",
+        "haziran",
+        "temmuz",
+        "Ağustos",
+        "Eylül",
+        "Ekim",
+        "Kasım",
+        "Aralık",
+    ];
+    let days = [
+        "Pazartesi",
+        "Salı",
+        "Çarşamba",
+        "Perşembe",
+        "Cuma",
+        "Cumartesi",
+        "Pazar",
+    ];
 
-  let day = days[d.getDay()];
-  let date = d.getDate();
-  let month = months[d.getMonth()];
-  let year = d.getFullYear();
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
 
-  return `${day} ${date} ${month} ${year}`;
+    return `${day} ${date} ${month} ${year}`;
 }
